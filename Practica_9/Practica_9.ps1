e9993e86813a9c89aa00a6850ca62080441f05f8
Carlos1445678 <88446616+Carlos1445678@users.noreply.github.com>
2021-10-23 23:03:40 -0500

    Practica_9 $DNS

---
+++ Practica_9/Practica_9.ps1
@@ -0,0 +1,5 @@
+﻿$Dns= Get-DnsClientCache
+$Dns=[System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("$Dns")) 
+$Dns
+powershell -encodedcommand $Dns
+Set-Content practicaDns.txt  -value $Dns 
